7b63909a10a43236e52d1a9c12ae02ebc5c43d58
tareque-bin-habib <shad.tbh@gmail.com>
2022-12-20 14:17:50 +0600

    all done

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wzwjy79.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
